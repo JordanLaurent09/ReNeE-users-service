78bc0609de3a3ca94869d33fcb000bc5ad553695
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2025-07-29 21:27:36 +0300

    add swagger to controllers

--- Resources/Users/UsersController.cs
+++ Resources/Users/UsersController.cs
@@ -6,7 +6,9 @@ using users_service.Resources.Users.Interfaces;
 
 namespace users_service.Resources.Users
 {
+    [ApiController]
     [Route("users")]
+    [Produces("application/json")]
     public class UsersController : Controller
     {
         private IUserService _userService;
@@ -20,7 +22,11 @@ namespace users_service.Resources.Users
         /// Gets all existing users
         /// </summary>
         /// <returns></returns>
+        /// <response code="200">Returns all users</response>
+        /// <response code="500">If server error occurs</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Route("all-user")]
         public IActionResult GetAll()
         {
@@ -32,7 +38,15 @@ namespace users_service.Resources.Users
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
+        /// <response code="200">Returns specific user by id</response>
+        /// <response code="400">If param is absent</response>
+        /// <response code="404">If specific user not found</response>
+        /// <response code="500">If server error occurs</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Route("{id:int}")]
         public IActionResult GetById(int id)
         {
@@ -44,7 +58,24 @@ namespace users_service.Resources.Users
         /// </summary>
         /// <param name="credentialsDTO"></param>
         /// <returns></returns>
+        /// <remarks>
+        /// 
+        /// {
+        ///     "credential": "test-mail@mail.test",
+        ///     "password": "secret_word"
+        /// }
+        /// 
+        /// 
+        /// </remarks>
+        /// <response code="200">The specific user has been authenticated</response>
+        /// <response code="400">If entity is null</response>
+        /// <response code="404">If specific user not found</response>
+        /// <response code="500">If server error occurs</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Route("login")]
         public IActionResult GetByCredential([FromBody] CredentialsDTO credentialsDTO)
         {           
@@ -57,7 +88,25 @@ namespace users_service.Resources.Users
         /// </summary>
         /// <param name="newUser"></param>
         /// <returns></returns>
+        /// <remarks>
+        /// 
+        ///  {
+        ///     "login": "fred21",
+        ///     "firstname": "Fred",
+        ///     "lastname": "Johnson",
+        ///     "email": "freddie22@mail.com",
+        ///     "sex": "MALE",
+        ///     "password": "iamfreddie"
+        ///  }
+        /// 
+        /// </remarks>
+        /// <response code="201">Returns newly created user</response>
+        /// <response code="400">If entity is null</response>
+        /// <response code="500">If server error occurs</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Route("new")]
         public IActionResult AddNew([FromBody] User newUser)
         {                      
@@ -70,7 +119,15 @@ namespace users_service.Resources.Users
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
+        /// <response code="200">Deletes specific user by id</response>
+        /// <response code="400">If param is absence</response>
+        /// <response code="404">If user not found</response>
+        /// <response code="500">If server error occurs</response>
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Route("{id:int}")]
         public IActionResult DeleteById(int id)
         {
@@ -84,7 +141,28 @@ namespace users_service.Resources.Users
         /// </summary>
         /// <param name="changedEntity"></param>
         /// <returns></returns>
+        /// <remarks>
+        /// 
+        /// {
+        ///     "id": 10,
+        ///     "login": "fred21",
+        ///     "firstname": "Fred",
+        ///     "lastname": "Johnson",
+        ///     "email": "freddie22@mail.com",
+        ///     "sex": "MALE",
+        ///     "password": "iamfreddie"
+        ///  }
+        /// 
+        /// </remarks>
+        /// <response code="200">Returns updated user</response>
+        /// <response code="400">If entity is null</response>
+        /// <response code="404">If user not found</response>
+        /// <response code="500">If server error occurs</response>
         [HttpPatch]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Route("change-info")]
         public IActionResult Update([FromBody] User changedEntity)
         {
